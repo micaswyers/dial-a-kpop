d7bc704bc5ecd405462554f0d3a60ace280d09de
Mica Swyers <mswyers@twilio.com>
2019-01-16 21:45:53 -0800

    Remove mp4 files and change naming

--- convert_video.py
+++ convert_video.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 from pydub import (
     AudioSegment,
     playback,
@@ -8,11 +11,12 @@ from pytube import YouTube
 def convert_mp4_to_mp3(stream, file_handle):
     "Callback for converting downloaded mp4 to mp3"
 
-    filename = str(file_handle.name)
-    print(filename)
-    song = AudioSegment.from_file(filename, "mp4")
-    song.export(f"./assets/sotd.mp3", format="mp3")
-    # TODO remove mp4
+    mp4_filepath = str(file_handle.name)
+    song = AudioSegment.from_file(mp4_filepath, "mp4")
+    filename = re.split("[/\/.]", mp4_filepath)[3]
+    song.export(f"./assets/{filename}.mp3", format="mp3")
+    # Clean up mp4 file
+    os.remove(mp4_filepath)
     # How about here? Does something need to get returned?
 
 
@@ -20,7 +24,7 @@ def upload_to_assets(filename):
     pass
 
 
-def convert_link_to_mp4(yt_link=None):
+def convert_link_to_mp4(yt_link=None, asset_name='sotd'):
     "Given a YT link, downloads mp4 file in ./assets"
 
     if not yt_link:
@@ -28,5 +32,5 @@ def convert_link_to_mp4(yt_link=None):
     yt = YouTube(yt_link)
     yt.register_on_complete_callback(convert_mp4_to_mp3)
     stream = yt.streams.first()
-    stream.download('./assets', 'sotd')
+    stream.download('./assets', asset_name)
     # Do I return here? I continue to be confused by callbacks.
