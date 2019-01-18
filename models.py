24ee1dcf4687e8a276d5b8213018e2c72030ad0c
Mica Swyers <mswyers@twilio.com>
2019-01-18 14:22:45 -0800

    Models, manage files

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+from sqlalchemy.sql import func
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    asset_url = db.Column(db.String(), nullable=False)
+    title = db.Column(db.String())
+    artist = db.Column(db.String())
+    video_url = db.Column(db.String(), nullable=False)
+    date_added = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    date_last_played = db.Column(db.DateTime(timezone=True))
+
+    def __repr__(self):
+        return f"Song {self.id}"
