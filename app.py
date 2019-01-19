f93935b95cd89ae3991795d91ea375f7ab5ff4e0
Mica Swyers <mswyers@twilio.com>
2019-01-18 16:40:48 -0800

    Add hello world route

--- app.py
+++ app.py
@@ -18,7 +18,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Song
+#from models import Song
+
+@app.route("/")
+def hello():
+    return "Hello World"
 
 @app.route("/voice", methods=['GET', 'POST'])
 def answer_call():
