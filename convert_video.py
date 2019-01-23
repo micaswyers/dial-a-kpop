dd752917338fd081e92cd4d1a204148fc9a8e8f0
Mica Swyers <micaswyers@gmail.com>
2019-01-22 18:04:25 -0800

    Add argparser to convert_video script

--- convert_video.py
+++ convert_video.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import os
 import re
 
@@ -8,7 +9,7 @@ from pydub import (
 from pytube import YouTube
 
 
-def convert_mp4_to_mp3(stream, file_handle):
+def _convert_mp4_to_mp3(stream, file_handle):
     "Callback for converting downloaded mp4 to mp3"
 
     mp4_filepath = str(file_handle.name)
@@ -24,13 +25,39 @@ def upload_to_assets(filename):
     pass
 
 
-def convert_link_to_mp4(yt_link=None, asset_name='sotd'):
+def convert_link(yt_link=None, asset_name='sotd'):
     "Given a YT link, downloads mp4 file in ./assets"
 
     if not yt_link:
         return "You need to specify a YouTube link."
     yt = YouTube(yt_link)
-    yt.register_on_complete_callback(convert_mp4_to_mp3)
+    yt.register_on_complete_callback(_convert_mp4_to_mp3)
     stream = yt.streams.first()
     stream.download('./assets', asset_name)
     # Do I return here? I continue to be confused by callbacks.
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--link',
+        type=str,
+        dest='link',
+        default='',
+        action='store',
+        required=True,
+        help="Address of YouTube video from which to extract audio",
+    )
+
+    parser.add_argument(
+        '--asset',
+        type=str,
+        dest='asset_name',
+        default='',
+        action='store',
+        required=False,
+        help="Name of asset file [artist_song_title]",
+    )
+
+    args = parser.parse_args()
+
+    convert_link(yt_link=args.link, asset_name=args.asset_name)
