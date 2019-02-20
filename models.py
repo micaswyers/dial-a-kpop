65217fa7b86f822dc24f862c92ec3d24f2d2920d
Mica Swyers <mswyers@twilio.com>
2019-02-19 19:14:25 -0800

    Remove korean_artist col

--- models.py
+++ models.py
@@ -9,7 +9,6 @@ class Song(db.Model):
     asset_url = db.Column(db.String(), nullable=False)
     title = db.Column(db.String())
     artist = db.Column(db.String())
-    korean_artist = db.Column(db.String())
     video_url = db.Column(db.String(), nullable=False)
     date_added = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
